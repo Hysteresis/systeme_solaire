0381252c147e81d9de2822bc15f9da9a4656d45c
Legenda\Pierrot <piballz@gmail.com>
2023-01-29 19:55:14 +0100

    animation navbar

--- script/navbarActive.js
+++ script/navbarActive.js
@@ -1,13 +1,57 @@
 $(document).ready(function () {
-    var url = window.location;
-    // console.log(url);
+    var url = $(location).attr("href"); 
+    var indexDebut = url.indexOf('uc=');
+    console.log(indexDebut);
+    console.log(url.substr(indexDebut));
 
-    $('ul.nav a[href="' + url + '"]').parent().addClass('active');
-    $('ul.nav a').filter(function () {
-        console.log(this.href);
 
-        return this.href == url;
+    if(url.substr(indexDebut) == "uc=planet"){
+        $("#item0").addClass("active");
+        $("#item0").css("color", "gold");
+        $("#item0").css("text-decoration", "underline");
+        $("#item0").css("text-underline-position", "under");
+        $("#item0").css("text-underline-offset", "5px");
+    }
+    if(url.substr(indexDebut) == "uc=dwarfPlanet"){
+        $("#item1").addClass("active");
+        $("#item1").css("color", "gold");
+        $("#item1").css("text-decoration", "underline");
+        $("#item1").css("text-underline-position", "under");
+        $("#item1").css("text-underline-offset", "5px");
+    }
+    if(url.substr(indexDebut) == "uc=moon"){
+        $("#item2").addClass("active");
+        $("#item2").css("color", "gold");
+        $("#item2").css("text-decoration", "underline");
+        $("#item2").css("text-underline-position", "under");
+        $("#item2").css("text-underline-offset", "5px");
+    }
+    if(url.substr(indexDebut) == "uc=asteroid"){
+        $("#item3").addClass("active");
+        $("#item3").css("color", "gold");
+        $("#item3").css("text-decoration", "underline");
+        $("#item3").css("text-underline-position", "under");
+        $("#item3").css("text-underline-offset", "5px");
+    }
+    if(url.substr(indexDebut) == "uc=comet"){
+        $("#item4").addClass("active");
+        $("#item4").css("color", "gold");
+        $("#item4").css("text-decoration", "underline");
+        $("#item4").css("text-underline-position", "under");
+        $("#item4").css("text-underline-offset", "5px");
+    }
+    if(url.substr(indexDebut) == "uc=star"){
+        $("#item5").addClass("active");
+        $("#item5").css("color", "gold");
+        $("#item5").css("text-decoration", "underline");
+        $("#item5").css("text-underline-position", "under");
+        $("#item5").css("text-underline-offset", "5px");
+    }
 
-    }).parent().addClass('active');
+
+    $(window).ready(function () {
+        // executes when complete page is fully loaded, including all frames, objects and images
+        $(".spinnerLoader").addClass("visually-hidden");
+    });
 
 });
--- vue/navbar.php
+++ vue/navbar.php
@@ -10,22 +10,22 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?uc=planet">Planètes</a>
+                    <a id="item0" class="nav-link" href="index.php?uc=planet">Planètes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?uc=dwarfPlanet">Planètes naines</a>
+                    <a id="item1" class="nav-link" href="index.php?uc=dwarfPlanet">Planètes naines</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?uc=moon">Lunes</a>
+                    <a id="item2" class="nav-link" href="index.php?uc=moon">Lunes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?uc=asteroid">Asteroïdes</a>
+                    <a id="item3" class="nav-link" href="index.php?uc=asteroid">Asteroïdes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?uc=comet">Comètes</a>
+                    <a id="item4" class="nav-link" href="index.php?uc=comet">Comètes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?uc=star">Etoile</a>
+                    <a id="item5" class="nav-link" href="index.php?uc=star">Etoile</a>
                 </li>
                 <form method="get" class="d-flex" action="index.php">
                     <input class="form-control me-2" name="searchName" type="search" placeholder="Planète, comète, lune,..." aria-label="Search">
